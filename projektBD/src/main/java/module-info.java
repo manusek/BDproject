682c7da9dda8a197736e0162903539aa5db2fbc5
flor3kk <szkolny.7883@gmail.com>
2024-03-20 13:10:10 +0100

    NOWY PROJ

---
+++ projektBD/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.projektbd {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.projektbd to javafx.fxml;
+    exports com.example.projektbd;
+}
