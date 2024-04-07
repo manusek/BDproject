99ad18eb9feb7f8e9cef01d5273304ab79e4951b
flor3kk <szkolny.7883@gmail.com>
2024-04-07 23:05:18 +0200

    login

--- projektBD/src/main/java/com/example/projektbd/Login.java
+++ projektBD/src/main/java/com/example/projektbd/Login.java
@@ -1,4 +1,24 @@
 package com.example.projektbd;
 
-public class Login {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
